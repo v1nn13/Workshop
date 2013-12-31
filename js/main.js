f7efd110c084e3014b9f4bb11e05679bf66f8f0b
v1nn13 <v1nn13@hotmail.co.uk>
2013-12-31 14:48:55 +0000

    Done some more stuff

--- js/main.js
+++ js/main.js
@@ -1,22 +1,9 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
     initialize: function () {
         var self = this;
-        this.store = new MemoryStore(function() {
-            self.renderHomeView();
+        this.store = new MemoryStore(function () {
+            $('body').html(new HomeView(self.store).render().el);
         });
     },
     
@@ -26,17 +13,6 @@ var app = {
         } else {
             alert(title ? (title + ': ' + message) : message);
         }
-    },
-    
-    renderHomeView: function() {
-        var html = 
-            '<div class="header"><h1>Home</h1></div>' +
-            '<div class="search-view">' +
-            '<input class="search-key" />' +
-            '<ul class="employee-list"></ul>' +
-            '</div>';
-        $('body').html(html);
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
