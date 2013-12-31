83b8420445604e4b6a415a5167ab7d1c06aa56fa
v1nn13 <v1nn13@hotmail.co.uk>
2013-12-31 13:56:21 +0000

    Added an alert method and called on app init

--- js/main.js
+++ js/main.js
@@ -13,9 +13,20 @@ var app = {
         });
     },
 
-    initialize: function() {
-        this.store = new MemoryStore();
+    initialize: function () {
+        var self = this;
+        this.store = new MemoryStore(function() {
+            self.showAlert('Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+    
+    showAlert: function(message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ': ' + message) : message);
+        }
     }
 
 };
